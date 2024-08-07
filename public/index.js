7da603ec818ccc7a44d9fbb2ab895ce97b85c58c
ikoamu <ikoamu@gmail.com>
2024-08-08 05:53:04 +0900

    feat: side-clipboard

--- public/index.js
+++ public/index.js
@@ -1,49 +1,92 @@
-const vscode = acquireVsCodeApi();
+(function () {
+  const vscode = acquireVsCodeApi();
+  const previousState = vscode.getState();
 
-let items = [];
+  let items = previousState?.items ?? [];
 
-function addItem() {
-  const input = document.getElementById("newItem");
-  const text = input.value.trim();
-  if (text) {
-    vscode.postMessage({ type: "addItem", text: text });
-    input.value = "";
-  }
-}
-
-function deleteItem(id) {
-  vscode.postMessage({ type: "deleteItem", id: id });
-}
-
-function copyToClipboard(text) {
-  vscode.postMessage({ type: "copyToClipboard", text });
-}
-
-function pasteToTerminal(text) {
-  vscode.postMessage({ type: "pasteToTerminal", text });
-}
-
-function updateItems() {
-  const list = document.getElementById("itemList");
-  list.innerHTML = "";
-  items.forEach((item) => {
-    const li = document.createElement("li");
-    li.innerHTML = `<span>${item.text}</span>
-      <button onclick="copyToClipboard('${item.text}')">copy</button>
-      <button onclick="pasteToTerminal('${item.text}')">paste</button>
-      <button onclick="deleteItem('${item.id}')">Delete</button>`;
-    list.appendChild(li);
+  document.getElementById("newItem").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const input = document.getElementById("newItem");
+      const text = input.value.trim();
+      addItem(text);
+      input.value = "";
+    }
   });
-}
 
-window.addEventListener("message", (event) => {
-  const message = event.data;
-  switch (message.type) {
-    case "updateItems":
-      items = message.items;
-      updateItems();
-      break;
+  function addItem(text) {
+    if (!text) {
+      return;
+    }
+    items.push({ id: Date.now().toString(), text });
+    vscode.setState({ items });
+    updateItems();
   }
-});
 
-vscode.postMessage({ type: "getItems" });
+  function deleteItem(id) {
+    items = items.filter((item) => item.id !== id);
+    vscode.setState({ items });
+    updateItems();
+  }
+
+  function copyToClipboard(text) {
+    vscode.postMessage({ type: "copyToClipboard", text });
+  }
+
+  function pasteToTerminal(text) {
+    vscode.postMessage({ type: "pasteToTerminal", text });
+  }
+
+  function updateItems() {
+    const list = document.getElementById("itemList");
+    list.innerHTML = "";
+    items.forEach((item) => {
+      const li = document.createElement("li");
+
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("trash-button");
+      deleteButton.innerHTML = `<i class="codicon codicon-close"></i>`;
+      deleteButton.addEventListener("click", () => deleteItem(item.id));
+      li.appendChild(deleteButton);
+
+      const codeBlockContainer = document.createElement("div");
+      codeBlockContainer.classList.add("side-clipboard-code-block-container");
+
+      const codeBlock = document.createElement("div");
+      codeBlock.classList.add("side-clipboard-code-block");
+      codeBlock.textContent = item.text;
+      codeBlockContainer.appendChild(codeBlock);
+
+      const actionButtonsContainer = document.createElement("div");
+      actionButtonsContainer.classList.add("action-buttons-container");
+
+      const copyButton = document.createElement("button");
+      copyButton.classList.add("action-button");
+      copyButton.innerHTML = `<i class="codicon codicon-copy"></i>`;
+      copyButton.addEventListener("click", () => copyToClipboard(item.text));
+      actionButtonsContainer.appendChild(copyButton);
+
+      const pasteButton = document.createElement("button");
+      pasteButton.classList.add("action-button");
+      pasteButton.innerHTML = `<i class="codicon codicon-terminal"></i>`;
+      pasteButton.addEventListener("click", () => pasteToTerminal(item.text));
+      actionButtonsContainer.appendChild(pasteButton);
+      codeBlockContainer.appendChild(actionButtonsContainer);
+      li.appendChild(codeBlockContainer);
+
+      list.appendChild(li);
+    });
+  }
+
+  window.addEventListener("message", (event) => {
+    const message = event.data;
+    switch (message.type) {
+      case "addItem": {
+        console.log("addItem", message.text);
+        addItem(message.text);
+        break;
+      }
+    }
+  });
+
+  updateItems();
+})();
--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,135 @@
 import * as vscode from 'vscode';
-import { ItemViewProvider } from './ItemViewProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-
-
+	const provider =new ItemsViewProvider(context.extensionUri);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(
-			'sideClipboardView',
-			new ItemViewProvider(context.extensionUri)
+			ItemsViewProvider.viewType,
+			provider
 		),
+	);
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'sideClipboard.addItemFromClipboard',
+			() => {
+				provider.addItemFromClipboard();
+			}
+		)
 	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+export class ItemsViewProvider implements vscode.WebviewViewProvider {
+  private _view?: vscode.WebviewView;
+
+  public static readonly viewType = 'sideClipboard.itemsView';
+
+  constructor(private readonly extensionUri: vscode.Uri) {}
+
+  public addItemFromClipboard() {
+    vscode.env.clipboard.readText().then(text => {
+      if (!text || !this._view) {
+        return;
+      }
+
+      this._view?.webview.postMessage({
+        type: 'addItem',
+        text,
+      });
+    });
+  }
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+  ) {
+    this._view = webviewView;
+        
+    const scriptUri = webviewView.webview.asWebviewUri(
+      vscode.Uri.joinPath(this.extensionUri, "public", "index.js")
+    );
+    const styleUri = webviewView.webview.asWebviewUri(
+      vscode.Uri.joinPath(this.extensionUri, "public", "index.css")
+    );
+    const codiconsUri = webviewView.webview.asWebviewUri(
+      vscode.Uri.joinPath(this.extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css')
+    );
+
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.joinPath(this.extensionUri, 'public'),
+        vscode.Uri.joinPath(this.extensionUri, 'node_modules', '@vscode/codicons', 'dist')
+      ]
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(
+      webviewView.webview, scriptUri, styleUri, codiconsUri
+    );
+
+    webviewView.webview.onDidReceiveMessage(data => {
+      switch (data.type) {
+        case 'copyToClipboard':
+          this._copyToClipboard(data.text);
+          break;
+        case 'pasteToTerminal':
+          this._pasteToTerminal(data.text);
+          break;
+        }
+      });
+    }
+
+    private _getHtmlForWebview(webview: vscode.Webview, scriptUri: vscode.Uri, styleUri: vscode.Uri, codiconsUri: vscode.Uri) {
+      const nonce = getNonce();
+      return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; font-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="${styleUri}">
+        <link href="${codiconsUri}" rel="stylesheet" />
+      </head>
+      <body>
+        <input type="text" id="newItem" class="input" placeholder="New Item">
+        <ul id="itemList"></ul>
+        <script nonce="${nonce}" src="${scriptUri}"></script>
+      </body>
+      </html>`;
+    }
+
+    private _copyToClipboard(text: string) {
+      vscode.env.clipboard.writeText(text);
+      vscode.window.showInformationMessage(`📋Copied to clipboard \"${text}\"`);
+    }
+
+    private _pasteToTerminal(text: string) {
+      const paste = (t: vscode.Terminal) => {
+        t.sendText(text, false);
+        t.show();
+      };
+      
+      const activeTerminal = vscode.window.activeTerminal;
+      if (activeTerminal) {
+        paste(activeTerminal);
+        return;
+      }
+
+      const terminals = vscode.window.terminals;
+      if (terminals.length) {
+        paste(terminals[0]);
+      } else {
+        paste(vscode.window.createTerminal());
+      }
+    }
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
